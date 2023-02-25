36143c2bb86c490a92be1ae267a57a5ad448406d
Arsene <duplex.dissi00@gmail.com>
2023-02-25 02:30:27 -0500

    Premier commit du projet

---
+++ sac-et-chaussure/sac-et-chaussure/src/main/java/com/djonia/sacetchaussure/SacEtChaussureApplication.java
@@ -0,0 +1,13 @@
+package com.djonia.sacetchaussure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacEtChaussureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacEtChaussureApplication.class, args);
+	}
+
+}
---
+++ sac-et-chaussure/sac-et-chaussure/src/test/java/com/djonia/sacetchaussure/SacEtChaussureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djonia.sacetchaussure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacEtChaussureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
