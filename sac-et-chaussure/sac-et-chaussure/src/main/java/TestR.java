e37c4c46fdc1e5c0a93407f680972a2cb5a7c170
Arsene <duplex.dissi00@gmail.com>
2023-04-07 19:23:56 -0400

    Ma premiere modification comme flix

--- sac-et-chaussure/sac-et-chaussure/src/main/java/TestR.java
+++ sac-et-chaussure/sac-et-chaussure/src/main/java/TestR.java
@@ -2,6 +2,7 @@ public class TestR {
   public static void main(String[] args) {
 
     System.out.println("Bonjour ");
-    //
+    System.out.println("Comment tu t'appele ? ");
+    System.out.println("salut ");
   }
 }
